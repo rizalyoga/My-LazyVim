0edf587d879b897e751c82af04fe8f7e0c7195bd
rizalyoga <ach.rizal.yogaswara@gmail.com>
2024-04-16 16:34:23 +0700

    My LazyVim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    -- "rizalyoga/oh-lucy-custom.nvim",
+    -- "Yazeed1s/oh-lucy.nvim",
+    -- "bluz71/vim-nightfly-colors",
+    name = "tokyonight",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
